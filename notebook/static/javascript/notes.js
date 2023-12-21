4169c49ac4114ac9909404cd40f6f1a198d92dd6
sufyaan323 <sufyaan323@gmail.com>
2023-12-21 18:45:59 +0000

    new note animation cleanup

--- notebook/static/javascript/notes.js
+++ notebook/static/javascript/notes.js
@@ -1,6 +1,8 @@
 const form = document.querySelector(".addEntry")
 const open = document.querySelector(".newEntry");
 const close = document.querySelector(".close");
+const addButton = document.querySelector(".addButtonIdentifier")
+const cards = document.querySelector(".card")
 let formId = document.querySelector(".noteId");
 let formName = document.querySelector(".noteTitle");
 let formText = document.querySelector(".noteText");
@@ -8,17 +10,24 @@ let formText = document.querySelector(".noteText");
 open.addEventListener("click", () =>{
     form.classList.remove("hidden");
     form.classList.add("show");
+	addButton.classList.add("hidden");
+	cards.classList.add("hidden");
 
 });
 
 close.addEventListener("click", () => {
     form.classList.remove("show");
     form.classList.add("hidden");
+	addButton.classList.remove("hidden");
+	cards.classList.remove("hidden");
+
 });
 
 function edit(callId, callName, callText){
     form.classList.remove("hidden");
     form.classList.add("show");
+	addButton.classList.add("hidden");
+	cards.classList.add("hidden");
 
     formId.value = callId;
     formName.value = callName;
